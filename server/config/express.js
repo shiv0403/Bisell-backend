924bf04a66c49a8eb37559889f1b0e358d1b81bf
shiv0403 <shiv0403gupta@gmail.com>
2022-07-09 11:10:49 +0530

    cors settings

--- server/config/express.js
+++ server/config/express.js
@@ -7,12 +7,13 @@ module.exports = function (app) {
 
   app.use(
     cors({
-      origin: "https://main.d2alebxh4xewmr.amplifyapp.com/",
-      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      allowedHeaders: ["Authorization", "Content-Type"],
+      origin: "https://main.d2alebxh4xewmr.amplifyapp.com",
       credentials: true,
     })
   );
 
   dbConnection();
 };
+
+// methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//       allowedHeaders: ["Authorization", "Content-Type"],
